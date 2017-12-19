8f3c6a8ee4fea09f6d953c7bfd5ba3239e49c452
Reto Kaiser <reto@retokaiser.com>
2017-12-19 14:38:36 +0100

    debug version

--- docker-push.sh
+++ docker-push.sh
@@ -2,7 +2,19 @@
 set -e
 
 docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+
+echo $(docker run cargomedia/gearman --version)
+
+V1=$(docker run cargomedia/gearman --version)
+echo ${V1}
+
+V2=$(docker run cargomedia/gearman --version | perl -pe 's/.*gearmand ([\d\.]+).*/$1/')
+echo ${V2}
+
 VERSION=$(docker run cargomedia/gearman --version | perl -pe 's/.*gearmand ([\d\.]+).*/$1/')
+echo ${VERSION}
+echo foo:${VERSION}
+
 docker tag cargomedia/gearman:latest cargomedia/gearman:${VERSION}
 docker push cargomedia/gearman:latest
 docker push cargomedia/gearman:${VERSION}
